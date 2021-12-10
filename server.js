efe1932fbe2258058b926b5fc0e5630f7913660a
Dipti Rani <gdipti642@gmail.com>
2021-12-10 16:33:02 +0530

    done with the project

--- server.js
+++ server.js
@@ -4,11 +4,11 @@
 // init project
 var express = require('express');
 var app = express();
-
+const util = require('util');
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +21,55 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api', (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: new Date(date).getTime(),
+    utc: new Date(date).toUTCString()
+  })
+});
 
+app.get('/api/:date?', (req, res) => {
+  const date = isNaN(+req.params.date) ? req.params.date : +req.params.date;
+
+  console.log(date, typeof date)
+  const dateInMs = new Date(date).getTime()
+  return !isNaN(dateInMs) ? res.json({
+    unix: new Date(date).getTime(),
+    utc: new Date(date).toUTCString()
+  }) : res.json({
+    error: "Invalid Date"
+  })
+  // let date = req.params.date;
+  // // // console.log(req.params.date.indexOf('-') > -1);
+  // // if(date.indexOf('-') < 0 || date.indexOf(':') < 0 || date.indexOf('/') < 0 || date.indexOf(',') < 0){
+  // if (!util.types.isDate(new Date(date))) {
+  //   date = parseInt(date);
+  // } else {
+  //   date = date;
+  // }
+
+  // // console.log(isNaN(date.parseInt));
+
+  // console.log(date);
+  // if (!isNaN(new Date(date).getTime())) {
+  //   // let parseDate = parseInt(req.params.date);
+  //   res.json({
+  //     unix: new Date(date).getTime(),
+  //     utc: new Date(date).toUTCString()
+  //   })
+  // }
+  // else {
+  //   res.json({
+  //     error: "Invalid Date"
+  //   })
+  // }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3002, function () {
+  console.log('Your app is listening on port ' + 3002);
 });
